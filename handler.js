e7edba96702888c85dfa875d1ca9aa252c59e6f4
vedri45 <vedri45@gmail.com>
2024-07-04 09:07:39 +0700

    Changed ollama to google generative-ai with Gemini model

--- handler.js
+++ handler.js
@@ -1,23 +1,23 @@
 require('dotenv').config();
 const { MessageMedia } = require('whatsapp-web.js');
 const fetch = require("node-fetch");
-const { Ollama } = require('ollama');
-const ollama = new Ollama({ host: `${process.env.OLLAMA_HOST}` })
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
 const waitTime = 60 * 1000;
 let lastFetchTime = null;
 
-const handleWaifuRequest = async (msg, client, body) => {
+const handleWaifuRequest = async (msg, body) => {
     if (!lastFetchTime || Date.now() - lastFetchTime >= waitTime) {
         try {
             const imageUrl = await fetch(`https://api.waifu.pics/sfw/${body}`)
                 .then(res => res.json())
                 .then(data => data.url);
             const media = await MessageMedia.fromUrl(imageUrl);
-            await client.sendMessage(msg.from, media);
+            await msg.reply(media, msg.from);
         } catch (error) {
             console.error('Error fetching image:', error);
-            client.sendMessage(msg.from, 'Oops! Something went wrong fetching your image.');
+            msg.reply('Oops! Something went wrong fetching your image.', msg.from);
         } finally {
             lastFetchTime = Date.now();
         }
@@ -25,11 +25,11 @@ const handleWaifuRequest = async (msg, client, body) => {
         const cooldownSeconds = waitTime / 1000;
         const remainingSeconds = Math.ceil(cooldownSeconds - (Date.now() - lastFetchTime) / 1000);
 
-        client.sendMessage(msg.from, `Mohon tunggu ${remainingSeconds} detik`);
+        msg.reply(`Mohon tunggu ${remainingSeconds} detik`, msg.from);
     }
 };
 
-const handleHSRImageRequest = async (msg, client, body, collection, tag) => {
+const handleHSRImageRequest = async (msg, body, collection, tag) => {
     const capitalizeStr = body.charAt(0).toUpperCase() + body.slice(1);
     const pipeline = [
         { $match: { tag: `${capitalizeStr} (${tag})` } },
@@ -46,11 +46,11 @@ const handleHSRImageRequest = async (msg, client, body, collection, tag) => {
                     },
                 }).then(res => res.json());
                 const media = await MessageMedia.fromUrl(imageUrl.large);
-                await client.sendMessage(msg.from, media);
+                await msg.reply(media, msg.from);
             }
             catch (error) {
                 console.error('Error fetching image:', error);
-                client.sendMessage(msg.from, 'Oops! Something went wrong fetching your image.');
+                msg.reply('Oops! Something went wrong fetching your image.', msg.from);
             } finally {
                 lastFetchTime = Date.now();
             }
@@ -58,7 +58,7 @@ const handleHSRImageRequest = async (msg, client, body, collection, tag) => {
             const cooldownSeconds = waitTime / 1000;
             const remainingSeconds = Math.ceil(cooldownSeconds - (Date.now() - lastFetchTime) / 1000);
 
-            client.sendMessage(msg.from, `Mohon tunggu ${remainingSeconds} detik`);
+            msg.reply(`Mohon tunggu ${remainingSeconds} detik`, msg.from);
         }
     }
 };
@@ -91,28 +91,36 @@ const handleStickerRequest = async (msg) => {
 };
 
 const handlePromptRequest = async (msg) => {
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
     const prompt = msg.body.slice(8).trim();
-    try {
-        // Generate text using Ollama's gemma model
-        const generatedText = await ollama.chat({
-            model: process.env.OLLAMA_MODEL,
-            messages: [{ role: 'user', content: prompt }],
-        });
+    if (!lastFetchTime || Date.now() - lastFetchTime >= waitTime) {
+        try {
+            const result = await model.generateContent(prompt);
+            const response = await result.response;
+            const text = response.text();
 
-        if (generatedText.message.content.length === 0) {
-            await msg.reply('Sorry, the generated text is empty. Please try again with a different prompt.');
-            return;
+            if (text.length === 0) {
+                await msg.reply('Sorry, the generated text is empty. Please try again with a different prompt.');
+                return;
+            }
+
+            if (text.length > 2000) {
+                await msg.reply('Sorry, the generated text is too long. Please try again with a shorter prompt.');
+                return;
+            }
+
+            await msg.reply(text);
+        } catch (error) {
+            console.error('Error generating text with Ollama:', error);
+            await msg.reply('Sorry, there was an error generating text. Please try again later.');
+        } finally {
+            lastFetchTime = Date.now();
         }
+    } else {
+        const cooldownSeconds = waitTime / 1000;
+        const remainingSeconds = Math.ceil(cooldownSeconds - (Date.now() - lastFetchTime) / 1000);
 
-        if (generatedText.message.content.length > 2000) {
-            await msg.reply('Sorry, the generated text is too long. Please try again with a shorter prompt.');
-            return;
-        }
-
-        await msg.reply(generatedText.message.content);
-    } catch (error) {
-        console.error('Error generating text with Ollama:', error);
-        await msg.reply('Sorry, there was an error generating text. Please try again later.');
+        msg.reply(`Mohon tunggu ${remainingSeconds} detik`, msg.from);
     }
 }
 
